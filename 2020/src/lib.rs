d41df2d62f68ca29c766dc1504cb569626ed63d0
Anett Seeker <noeddl@users.noreply.github.com>
2020-12-03 18:44:59 +0100

    Add function to read a file into a Vec

---
+++ 2020/src/lib.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::PathBuf;
+
+pub fn file2vec(path: &PathBuf) -> Vec<String> {
+    let f = File::open(path).expect("Failed to open file");
+    let vec = BufReader::new(f).lines().map(|line| line.unwrap()).collect();
+
+    vec
+}
